cd0b76c040b5762d1581686b6a54e44001b58fa3
Leandro <leandro.wait@gmail.com>
2014-10-20 02:40:00 -0200

    Signed-off-by: Leandro <leandro.wait@gmail.com>

---
+++ src/negocio/AdicionalExclusividade.java
@@ -0,0 +1,11 @@
+package negocio;
+
+public class AdicionalExclusividade implements CalculoDireitosAutorais {
+
+	@Override
+	public double calcularDireitosAutoraisMes(int codigoEditora, int mes) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ src/negocio/BonusVendaLivros.java
@@ -0,0 +1,11 @@
+package negocio;
+
+public class BonusVendaLivros implements CalculoDireitosAutorais {
+
+	@Override
+	public double calcularDireitosAutoraisMes(int codigoEditora, int mes) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
